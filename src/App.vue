1d239ab0260a2d6d1338697623ae25bd7689a12f
abdurrahmanekecik <abdurrahmanekecik@gmail.com>
2023-10-10 21:51:00 +0300

    Assets and Search Added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
-
-
-
-
 <template>
   <div id="app">
-    <input v-model="city" placeholder="Şehir adını girin" />
-    <BButton @click="getWeather" variant="primary">Sorgula</BButton>
-    <WeatherComponent v-if="weatherData" :data="weatherData" />
+    <BForm>
+      <BFormGroup id="input-group-2" label="City Name:" label-for="input-2">
+        <BFormInput id="input-2" class="mb-2 me-sm-2 mb-sm-0" v-model="city" placeholder="City Name Enter" required />
+      </BFormGroup>
+      <BButton @click="getWeather" variant="primary">Search</BButton>
+      <WeatherComponent v-if="weatherData" :data="weatherData" />
+    </BForm>
   </div>
 </template>
 
-
-
 <script>
+import WeatherComponent from './components/WeatherComponent.vue';
+import axios from 'axios';
 
+export default {
+  name: 'App',
+  components: {
+    WeatherComponent
+  },
+  data() {
+    return {
+      city: '',
+      weatherData: null
+    };
+  },
+  methods: {
+    async getWeather(event) {
+      event.preventDefault();
+
+      try {
+        console.log(process.env.VUE_APP_API_KEY);
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${this.city},US&appid={${process.env.VUE_APP_API_KEY}}&units=imperial`)
+        this.weatherData = response.data;
+      } catch (error) {
+        console.error("Hava durumu bilgisi alınırken bir hata oluştu:", error);
+      }
+    }
+  }
+};
 </script>
 
 <style>
---
+++ src/components/WeatherComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>{{ data.name }}</h2>
+        <p>Sıcaklık: {{ data.main.temp }}°C</p>
+        <p>Açıklama: {{ data.weather[0].description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WeatherComponent',
+    props: {
+        data: Object
+    }
+};
+</script>
